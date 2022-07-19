38acd43bf42e0e48ee159ee627b7a98bad57d44d
Nikita <nvk.mpei@gmail.com>
2022-07-20 01:22:45 +0300

    pep8

--- backend/foodgram_api/users/serializers.py
+++ backend/foodgram_api/users/serializers.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
--- backend/foodgram_api/users/views.py
+++ backend/foodgram_api/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-from rest_framework import permissions, status, viewsets
+from rest_framework import permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
